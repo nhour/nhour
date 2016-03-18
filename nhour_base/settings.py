a38218c6343078825420c7635ffec71017a35e31
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2016-03-18 16:26:28 +0200

    Static files

--- nhour_base/settings.py
+++ nhour_base/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
