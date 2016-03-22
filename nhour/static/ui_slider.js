957bf9419b530bf755d71ee23801715d0d305a0c
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2016-03-22 13:38:38 +0200

    Wait until the slider node is loaded

--- nhour/static/ui_slider.js
+++ nhour/static/ui_slider.js
@@ -1,17 +1,19 @@
-var slider = document.getElementById('slider');
-var selectedHours = document.getElementById('hours-selected');
+function createSlider() {
+    var slider = document.getElementById('slider');
+    var selectedHours = document.getElementById('hours-selected');
 
-noUiSlider.create(slider, {
-    start: 10,
-    behaviour: 'tap',
-    connect: 'lower',
-    range: {
-        'min':  0.5,
-        'max':  50
-    },
-    step: 0.5,
-});
-slider.noUiSlider.on('update', function(values, handle) {
-    selectedHours.textContent = values[handle]
-    hours.value = values[handle]
-});
+    noUiSlider.create(slider, {
+        start: 10,
+        behaviour: 'tap',
+        connect: 'lower',
+        range: {
+            'min':  0.5,
+            'max':  50
+        },
+        step: 0.5,
+    });
+    slider.noUiSlider.on('update', function(values, handle) {
+        selectedHours.textContent = values[handle]
+        hours.value = values[handle]
+    });
+}
