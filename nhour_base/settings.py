4a24134efb38ea7562dcb689a10190ebe7aace79
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2016-04-25 14:34:55 +0300

    Requirements.txt version numbers

--- nhour_base/settings.py
+++ nhour_base/settings.py
@@ -52,6 +52,22 @@ MIDDLEWARE_CLASSES = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 ROOT_URLCONF = 'nhour.urls'
 
 TEMPLATES = [
