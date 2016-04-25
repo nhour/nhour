dbd2e070bce490117f8e43c05c70c5ee5286df30
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2016-04-25 14:13:35 +0300

    Config tweaking 2

--- nhour_base/settings.py
+++ nhour_base/settings.py
@@ -139,4 +139,4 @@ LOGIN_REDIRECT_URL = "/"
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
